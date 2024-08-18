22465e5ef5e856093e436a514efd2b21ef10bf41
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-18 21:15:57 +0100

    Update start.js

--- commands/prefix/start.js
+++ commands/prefix/start.js
@@ -1,33 +1,35 @@
 const { initializeUser, saveUserData, getUser, getAllUsers } = require('../dataManager');
+const { ActivityType } = require('discord.js');
 
 async function updateBotStatus(client) {
     const userCount = getAllUsers().length;
-    await client.user.setActivity(`${userCount} users enjoying their virtual life!`, { type: 'PLAYING' });
+    await client.user.setActivity(`${userCount} users enjoying their virtual life!`, { type: ActivityType.Playing });
 }
 
 module.exports = {
     name: 'start',
     description: 'Start your mining empire.',
-    async execute(message) {
-        const userId = message.author.id;
-        const username = message.author.username; // Fetch the username from the message author
+    async execute(interaction) {
+        const userId = interaction.user.id;
+        const username = interaction.user.username;
 
         if (!getUser(userId)) {
-            initializeUser(userId, username); // Pass both userId and username to initializeUser
+            // Initialize the user in the database
+            initializeUser(userId, username);
             await saveUserData();
 
             try {
-                await message.author.send('Welcome to your virtual life! Use "v map" to see your current location.');
+                await interaction.user.send('Welcome to your virtual life! Use "/map" to see your current location.');
             } catch (error) {
-                console.error(`Could not send DM to ${message.author.tag}.\n`, error);
+                console.error(`Could not send DM to ${interaction.user.tag}.\n`, error);
             }
 
-            await message.reply(`You have now officially signed up to the mining world, ${userId}! Check your DM for more instructions.`);
+            await interaction.reply(`You have now officially signed up to the mining world, ${username}! Check your DM for more instructions.`);
             
             // Update bot status
-            await updateBotStatus(message.client);
+            await updateBotStatus(interaction.client);
         } else {
-            await message.reply('You are already in the game!');
+            await interaction.reply('You are already in the game!');
         }
     }
 };
