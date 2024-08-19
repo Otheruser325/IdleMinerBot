bbc6ab01e80d61f4ee368da6b589002d1e84c5fc
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-19 19:47:46 +0100

    Update start.js

--- commands/slash/start.js
+++ commands/slash/start.js
@@ -7,28 +7,34 @@ module.exports = {
         .setName('start')
         .setDescription('Start your mining empire.'),
     async execute(interaction) {
-        await interaction.deferReply(); // Acknowledge the interaction
+        // Acknowledge the interaction
+        await interaction.deferReply(); 
 
         const userId = interaction.user.id;
-        const username = interaction.user.username; // Fetch the username from the interaction user
+        const username = interaction.user.username;
 
-        // Fetch the user data asynchronously
-        const user = await getUser(userId);  
+        try {
+            // Fetch the user data asynchronously
+            const user = await getUser(userId);  
 
-        if (!user) {
-            await initializeUser(userId, username);
+            if (!user) {
+                await initializeUser(userId, username);
 
-            try {
-                await interaction.author.send('Welcome to Idle Miner! Use "im!help" to get started.');
-            } catch (error) {
-                console.error(`Could not send DM to ${message.author.tag}.\n`, error);
+                try {
+                    await interaction.user.send('Welcome to Idle Miner! Use "im!help" to get started.');
+                } catch (error) {
+                    console.error(`Could not send DM to ${interaction.user.tag}.\n`, error);
+                }
+
+                await interaction.editReply(`You have now officially signed up to the mining world, ${username}! Check your DM for more instructions.`);
+                
+                await updateBotStatus(interaction.client); // Update bot status after user initialization
+            } else {
+                await interaction.editReply('You are already in the game!');
             }
-
-            await interaction.editReply(`You have now officially signed up to the mining world, ${username}! Check your DM for more instructions.`);
-            
-            await updateBotStatus(interaction.client); // Update bot status after user initialization
-        } else {
-            await interaction.reply('You are already in the game!');
+        } catch (error) {
+            console.error('Error executing slash command:', error);
+            await interaction.editReply('There was an error executing this command!');
         }
     }
 };
