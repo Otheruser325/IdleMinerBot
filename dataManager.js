b2ba5403c5cbcf61a1b8bcdad0facbedecf036ec
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-19 18:57:07 +0100

    Update dataManager.js

--- dataManager.js
+++ dataManager.js
@@ -13,6 +13,10 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
+console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
+console.log('FIREBASE_PRIVATE_KEY:', process.env.FIREBASE_PRIVATE_KEY);
+
 // User-related functions
 
 // Initialize a new user in Firestore
