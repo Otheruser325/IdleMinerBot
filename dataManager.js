d9b0f47790bd38bdd17087ed24f6a01d94c9e3ae
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-19 18:44:57 +0100

    Update dataManager.js

--- dataManager.js
+++ dataManager.js
@@ -5,9 +5,10 @@ admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') // Ensure newlines are correctly handled
+        // Ensure newlines are correctly handled in the private key
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
     }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`
 });
 
 const db = admin.firestore();
