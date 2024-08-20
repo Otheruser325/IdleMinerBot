ab752da5a3770a3554eb3ee2c89076236f0a95ba
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-20 22:41:28 +0100

    Update getMineFactor.js

--- utils/getMineFactor.js
+++ utils/getMineFactor.js
@@ -1,3 +1,5 @@
+const mineFactors = require('../config/mineFactors.json').mines;
+
 module.exports = function getMineFactor(mineName) {
     const mine = mineFactors.find(m => m.MineName === mineName);
     return mine ? mine.Factor : 1; // Default to 1 if not found
