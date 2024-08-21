844f17a4657892c3288def2860d3bb1bd74ef668
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-21 15:54:49 +0100

    Update dataManager.js

--- dataManager.js
+++ dataManager.js
@@ -3,7 +3,7 @@ const serviceAccount = require('./config/serviceAccountKey.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+    databaseURL: 'https://idleminerapi-default-rtdb.firebaseio.com/'
 });
 
 const db = admin.database(); // Use .database() for Realtime Database
