7c53105c7708605e0cbd2292297d4f716d0fa205
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-21 16:19:41 +0100

    Update shaft.js

--- commands/slash/shaft.js
+++ commands/slash/shaft.js
@@ -147,7 +147,7 @@ async function handleBuy(interaction, user, currentMine, tier) {
         totalDeposit: 0 // Initialize totalDeposit for new shaft
     });
 
-    await updateUser(user.id, user);
+    await updateUser(userId, user);
 
     return interaction.reply(`Successfully purchased Shaft Tier ${tier} for ${numberFormat(shaftInfo.Cost)} Cash in the ${currentMine.MineName}.`);
 }
@@ -184,7 +184,7 @@ async function handleUpgrade(interaction, user, currentMine, tier) {
     shaft.capacityPerWorker = adjustedCapacity;
     shaft.workerWalkingSpeedPerSecond = nextShaftInfo.WorkerWalkingSpeedPerSecond;
 
-    await updateUser(user.id, user);
+    await updateUser(userId, user);
 
     return interaction.reply(`Successfully upgraded Shaft Tier ${tier} to Level ${nextLevel} for ${numberFormat(nextShaftInfo.Cost)} Cash in the ${currentMine.MineName}.`);
 }
