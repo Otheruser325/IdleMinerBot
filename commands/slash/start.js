195fbb20363997edef82582141f221778275a8a0
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-21 15:49:21 +0100

    Update start.js

--- commands/slash/start.js
+++ commands/slash/start.js
@@ -7,21 +7,19 @@ module.exports = {
         .setName('start')
         .setDescription('Start your mining empire.'),
     async execute(interaction) {
-        // Acknowledge the interaction
         await interaction.deferReply(); 
 
         const userId = interaction.user.id;
         const username = interaction.user.username;
 
         try {
-            // Fetch the user data asynchronously
             const user = await getUser(userId);  
 
             if (!user) {
                 await initializeUser(userId, username);
 
                 try {
-                    await interaction.user.send('Welcome to Idle Miner! Use "im!help" to get started.');
+                    await interaction.user.send('Welcome to Idle Miner! Use `/help` to get started.');
                 } catch (error) {
                     console.error(`Could not send DM to ${interaction.user.tag}.\n`, error);
                 }
